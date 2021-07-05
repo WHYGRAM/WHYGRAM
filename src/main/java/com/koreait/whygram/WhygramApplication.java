c267999ab439b6280be828acbb59d611c87eef45
tg2021 <vldzlqn1230@naver.com>
2021-07-05 16:04:59 +0900

    commit test

---
+++ src/main/java/com/koreait/whygram/WhygramApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.whygram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhygramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhygramApplication.class, args);
+    }
+
+}
