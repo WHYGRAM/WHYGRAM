1d53020e45f87132055721c0924803667ebbe340
tg2021 <vldzlqn1230@naver.com>
2021-07-06 19:27:25 +0900

    2021-07-06 sql CREATE

--- src/main/resources/query.sql
+++ src/main/resources/query.sql
@@ -1,24 +1,23 @@
-
 -- 테이블 생성
 
 CREATE TABLE users (
-    users_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    user_email VARCHAR(30) NOT NULL UNIQUE,
-    user_password VARCHAR(100) NOT NULL,
-    user_name VARCHAR(5) NOT NULL,
-    user_gender TINYINT(1) UNSIGNED,
-    user_date_birth DATE NOT NULL,
-    user_nickname VARCHAR(12) NOT NULL UNIQUE,
-    user_regdt DATETIME DEFAULT NOW()
-) comment'회원가입 정보';
+   users_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+   user_email VARCHAR(30) NOT NULL UNIQUE,
+   user_password VARCHAR(100) NOT NULL,
+   user_name VARCHAR(5) NOT NULL,
+   user_gender TINYINT(1) UNSIGNED,
+   user_date_birth DATE NOT NULL,
+   user_nickname VARCHAR(12) NOT NULL UNIQUE,
+   user_regdt DATETIME DEFAULT NOW()
+) COMMENT'회원가입 정보';
 
 CREATE TABLE auth (
-    user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    auth_code CHAR(5) comment '회원가입 인증코드, null이면 인증받은 상태, 값이 있으면 인증해야 되는 상태',
-    auth_quit TINYINT(1) UNSIGNED NOT NULL comment'회원탈퇴 여부 0-회원 1-탈퇴',
-    FOREIGN KEY(user_id) references users(users_id)
+  user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  auth_code CHAR(5) comment '회원가입 인증코드, null이면 인증받은 상태, 값이 있으면 인증해야 되는 상태',
+  auth_quit TINYINT(1) UNSIGNED NOT NULL comment'회원탈퇴 여부 0-회원 1-탈퇴',
+  FOREIGN KEY(user_id) references users(users_id)
 
-) comment'회원 권한';
+) COMMENT'회원 권한';
 
 CREATE TABLE mypage (
     mypage_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -26,22 +25,22 @@ CREATE TABLE mypage (
     mypage_img VARCHAR(50),
     mypage_introduction VARCHAR(150),
     FOREIGN KEY(user_id) references users(users_id)
-) comment'마이 페이지';
+) COMMENT'마이 페이지';
 
 CREATE TABLE feed (
-    feed_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    users_id INT NOT NULL AUTO_INCREMENT,
-    fedd_ctnt VARCHAR(2200) NOT NULL,
-    user_regdt DATETIME DEFAULT NOW(),
-    FOREIGN KEY(users_id) references users(users_id)
-) comment'게시물';
+  feed_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  users_id INT NOT NULL AUTO_INCREMENT,
+  fedd_ctnt VARCHAR(2200) NOT NULL,
+  user_regdt DATETIME DEFAULT NOW(),
+  FOREIGN KEY(users_id) references users(users_id)
+) COMMENT'게시물';
 
 CREATE TABLE video(
-    video_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    feed_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    video_ctnt VARCHAR(50) NOT NULL comment'영상',
-    FOREIGN KEY(feed_id) references feed(feed_id)
-) comment'게시물 영상';
+  video_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  feed_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  video_ctnt VARCHAR(50) NOT NULL comment'영상',
+  FOREIGN KEY(feed_id) references feed(feed_id)
+) COMMENT'게시물 영상';
 
 CREATE TABLE cmt (
     cmt_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -53,9 +52,16 @@ CREATE TABLE cmt (
     FOREIGN KEY(users_id) references users(users_id)
 );
 
-CREATE TABLE img (
+CREATE TABLE image (
     image_id INT UNSIGNED AUTO_INCREMENT,
-    feed_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT
+    feed_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    image_ctnt VARCHAR(36) NOT NULL COMMENT'사진'
+) COMMENT'게시물 사진';
+
+CREATE TABLE video (
+    video_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    feed_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    video_ctnt VARCHAR(36) NOT NULL COMMENT'영상'
 );
 
 CREATE TABLE fav (
@@ -63,4 +69,4 @@ CREATE TABLE fav (
     cmt_id INT UNSIGNED AUTO_INCREMENT,
     FOREIGN KEY(feed_id) references feed(feed_id),
     FOREIGN KEY(cmt_id) references cmt(cmt_id)
-) comment'게시물과 댓글 좋아요';
+) COMMENT'게시물과 댓글 좋아요';
