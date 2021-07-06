8f9133eef1459ea2a7ed76e4900f988e8437028c
HyeonJin-Jang <xxhyeonj1nxx@naver.com>
2021-07-06 21:04:40 +0900

    controller, model, mapperXML, mapper, html, common, service

--- src/main/java/com/koreait/whygram/Controller/UserController.java
+++ src/main/java/com/koreait/whygram/Controller/UserController.java
@@ -1,5 +1,6 @@
 package com.koreait.whygram.Controller;
 
+import com.koreait.whygram.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
