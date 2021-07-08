02e43115133f83292a79e194d84524f92e2d4b51
durk011@naver.com <rhfoqkq4>
2021-07-08 14:25:01 +0900

    static folder

--- src/main/java/com/koreait/whygram/service/UserService.java
+++ src/main/java/com/koreait/whygram/service/UserService.java
@@ -1,6 +1,7 @@
 package com.koreait.whygram.service;
 
 import com.koreait.whygram.mapper.UserMapper;
+import com.koreait.whygram.model.user.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     @Autowired UserMapper mapper;
+
 }
