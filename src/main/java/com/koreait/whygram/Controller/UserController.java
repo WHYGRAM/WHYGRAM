0685450631a2eb7e3aa915ad358c91b08e621024
tg2021 <vldzlqn1230@naver.com>
2021-07-08 22:09:31 +0900

    2021-07-08 join service logic test완료

--- src/main/java/com/koreait/whygram/Controller/UserController.java
+++ src/main/java/com/koreait/whygram/Controller/UserController.java
@@ -2,14 +2,15 @@ package com.koreait.whygram.Controller;
 
 import com.koreait.whygram.model.user.UserEntity;
 import com.koreait.whygram.service.UserService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/user")
@@ -19,12 +20,34 @@ public class UserController {
     private UserService service;
 
     @GetMapping("/join")
-    public void getJoin(UserEntity userEntity) {}
+    public void getJoin(UserEntity param, @RequestParam(value = "err", defaultValue = "0") int err, Model model) {
+
+        switch(err) {
+            case 1:
+                model.addAttribute("errMsg", "가입 실패 했습니다.");
+                break;
+            case 2:
+                model.addAttribute("errMsg", "잘못된 접근 입니다.");
+                break;
+        }
+
+    }
 
     @PostMapping("/join")
-    public String postJoin(UserEntity userEntity, @RequestParam String pwchk) {
+    public String postJoin(UserEntity param, @RequestParam String pwchk) {
 
-       service.insUsers(userEntity, pwchk);
+        int result = service.insUsers(param, pwchk);
+        switch (result) {
+            case 0: return "redirect:user/login"; // 완료
+            case 1: return "redirect:user/join?err=1"; // 가입 실패
+            case 2: return "redirect:user/join?err=2"; // 검사 실패
+        }
         return null;
+
+
+
     }
+
+    @GetMapping("/login")
+    public void getLogin(UserEntity param) {}
 }
--- src/main/java/com/koreait/whygram/service/UserService.java
+++ src/main/java/com/koreait/whygram/service/UserService.java
@@ -3,6 +3,7 @@ package com.koreait.whygram.service;
 import com.koreait.whygram.mapper.UserMapper;
 import com.koreait.whygram.model.user.UserDomain;
 import com.koreait.whygram.model.user.UserEntity;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,22 +13,24 @@ public class UserService {
     @Autowired UserMapper mapper;
 
     public int insUsers(UserEntity param, String pwchk) {
-        UserDomain domain = mapper.selIdChk(param);
-        int idChk = domain.getIdChk();
 
-        // 아이디 중복 검사
-        if( idChk == 0) {
+        // 아이디 중복 검사 + 비밀번호 검사
+        if(mapper.selIdChk(param).getIdChk() == 0 || param.getUsers_password() == pwchk) {
+            // 통과하면
+            String hashedPw = BCrypt.hashpw(param.getUsers_password(), BCrypt.gensalt());
+            param.setUsers_password(hashedPw);
 
-            // 비밀번호와 비밀번호 확인 일치한지 확인
-            if(param.getUsers_password() == pwchk) {
-                // 비밀번호 암호화
+            int result = mapper.insUsers(param);
 
-                return mapper.insUsers(param);
+            if (result < 1) {
+                return 1; // 아이디 중복 검사, 비밀번호 검사 통과 했지만 가입은 안됏음
             }
-        } else {
-            return 0;
+            return 0; //   아이디 중복 검사 , 비밀번호 검사 통과 한 후 가입도 완료됨
         }
+        return 2; // 아이디 중복이 되거나 비밀번호가 서로 틀림
+
 
 
     }
+
 }
