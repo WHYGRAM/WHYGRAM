bd85d66714d6953c73f58d8d3b883acf4ea8b2ed
tg2021 <vldzlqn1230@naver.com>
2021-07-08 15:12:22 +0900

    2021-07-08 join service logic

--- src/main/java/com/koreait/whygram/service/UserService.java
+++ src/main/java/com/koreait/whygram/service/UserService.java
@@ -12,20 +12,20 @@ public class UserService {
 
     public int insUsers(UserEntity param, String pwchk) {
 
+
         // 아이디 중복 검사
-        if(mapper.selIdChk(param) == 0) {
+        if(mapper.selIdChk(param).equals(0)) {
 
             // 비밀번호와 비밀번호 확인 일치한지 확인
             if(param.getUsers_password() == pwchk) {
-dddddddd
+
                 // 비밀번호 암호화
 
                 return mapper.insUsers(param);
             }
         } else {
-            return 0;
+
         }
-
-
+            return 0;
     }
 }
