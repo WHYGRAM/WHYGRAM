0ad4b4d16cfeb017792b5516006320823ca1b02d
tg2021 <vldzlqn1230@naver.com>
2021-07-08 11:25:26 +0900

    2021-07-08 join form

--- src/main/java/com/koreait/whygram/service/UserService.java
+++ src/main/java/com/koreait/whygram/service/UserService.java
@@ -1,8 +1,11 @@
 package com.koreait.whygram.service;
 
+import com.koreait.whygram.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    private int num = 1;
+
+    @Autowired UserMapper mapper;
 }
