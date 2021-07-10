a2022bb821d5d282a9aca62f8a234461e0898304
tg2021 <vldzlqn1230@naver.com>
2021-07-10 17:07:57 +0900

    2021-07-10 로그인 작업중(2)

--- src/main/java/com/koreait/whygram/Controller/UserController.java
+++ src/main/java/com/koreait/whygram/Controller/UserController.java
@@ -4,10 +4,11 @@ import com.koreait.whygram.model.user.UserEntity;
 import com.koreait.whygram.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
