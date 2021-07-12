59e9adcbbed061b4ef413c56787d4ce067c0ab33
tg2021 <vldzlqn1230@naver.com>
2021-07-12 23:23:47 +0900

    2021-07-12 모달창 작업중(1)

--- src/main/resources/static/js/startPage.js
+++ src/main/resources/static/js/startPage.js
@@ -12,3 +12,28 @@ function clicked(strSpnChked, strSpn, strSectnChecked, strSectn) {
 
 loginSpn.addEventListener('click', () => {clicked(loginSpn, joinSpn, loginSectn, joinSectn)});
 joinSpn.addEventListener('click', () => {clicked(joinSpn, loginSpn, joinSectn, loginSectn)});
+
+/********* 비밀번호 모달창 ********/
+const findPwElem = document.querySelector('#findPw');
+const modalFindPwElem = document.querySelector('.modal');
+const modalCloseIcon = document.querySelector('.modal_close_icon');
+const textchkElem = document.querySelector('.textchk');
+const chkbtnElem = document.querySelector('.chkbtn');
+
+// 모달창 열기
+findPwElem.addEventListener('click', () => {
+    modalFindPwElem.classList.remove('hide');
+});
+
+// 모달창 닫기
+
+if(findPwElem) {
+    modalCloseIcon.addEventListener('click', () => {
+        modalFindPwElem.classList.add('hide');
+    });
+}
+
+// 모달창에 입력값이 있으면 버튼 활성화
+textchkElem.addEventListener('keyup', () => {
+    chkbtnElem.classList.remove('disable');
+});
