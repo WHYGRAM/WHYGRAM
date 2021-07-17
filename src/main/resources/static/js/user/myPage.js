823f37040d127a964eeebb6e86b06196c184cc39
tg2021 <vldzlqn1230@naver.com>
2021-07-17 11:51:07 +0900

    2021-07-17 프로필작업중(2)

---
+++ src/main/resources/static/js/user/myPage.js
@@ -0,0 +1,7 @@
+const profileInputElem = document.querySelector('#profileInput');
+const profileImgBtnElem = document.querySelector('#profileImgBtn');
+
+
+profileImgBtnElem.addEventListener('click', () => {
+    profileInputElem.click();
+});
