7b6635868582c69e2d7c75c93cb46c9db9661524
Kimsw-G <65203794+Kimsw-G@users.noreply.github.com>
2021-07-26 12:07:52 +0900

    로그아웃 아이콘 하버

---
+++ src/main/resources/static/js/common/common.js
@@ -0,0 +1,2 @@
+const logoutElem = document.querySelector('.header__right.right__icon .logout');
+logoutElem.addEventListener('hover', () => {logoutElem.innerHTML = '<i class="bi bi-door-open-fill"></i>'});
