428b144bdb445431380e71921d2f0bb83ee66383
HyeonJin-Jang <xxhyeonj1nxx@naver.com>
2021-07-26 21:44:47 +0900

    common.js 함수 추가

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 function moveToMypage(uId) { location.href = '/profile/mypage?users_id=' + uId; }
-=======
+
 const logoutElem = document.querySelector('.header__right.right__icon .logout');
 logoutElem.addEventListener('hover', () => {logoutElem.innerHTML = '<i class="bi bi-door-open-fill"></i>'});
->>>>>>> origin/HJ
