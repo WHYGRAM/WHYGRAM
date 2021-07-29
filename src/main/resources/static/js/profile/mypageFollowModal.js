f7512c1c04dc99220781161072b93d5bea2b5e6a
Kimsw-G <65203794+Kimsw-G@users.noreply.github.com>
2021-07-29 16:20:44 +0900

    follow list

--- src/main/resources/static/js/profile/mypageFollowModal.js
+++ src/main/resources/static/js/profile/mypageFollowModal.js
@@ -1,14 +1,25 @@
-
 const followerElemArr = document.querySelectorAll('.followerCnt');
 const followElemArr = document.querySelectorAll('.followCnt');
 
 const followModalElem = document.querySelector('#followModal');
 const modalTitleElem = document.querySelector('#modalTitle');
 const modalListElem = document.querySelector('#modalList')
+
 if(followerElemArr) {
     followerElemArr.forEach(item => {
         item.addEventListener('click', () => {
             modalTitleElem.innerText = '팔로워';
+            
+            fetch(`followerList?follow_him=${mypageConstElem.dataset.pid}`)
+                .then(res => res.json())
+                .then(myJson => {
+                    if(myJson.length > 0) {
+                        myJson.forEach(item => {
+                            const cont = makeFollowItem(item);
+                            modalFollowItemConElem.append(cont);
+                        });
+                    }
+                });
         });
     });
 }
