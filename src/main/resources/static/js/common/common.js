e2b54581ccdd442a1036e05c721b6987c919e7b8
HyeonJin-Jang <xxhyeonj1nxx@naver.com>
2021-08-06 18:25:11 +0900

    hover, detail 하는 중

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -1,8 +1,5 @@
 function moveToMypage(uId) { location.href = '/profile/mypage?users_id=' + uId; }
 
-const imgIcon = document.createElement('i');
-imgIcon.className = 'bi bi-person-circle';
-
-const logoutElem = document.querySelector('.header__right>.right__icon>.logout');
+const logoutElem = document.querySelector('.logout');
 logoutElem.addEventListener('mouseout', () => {logoutElem.innerHTML = `<i class="header-icon bi bi-door-closed-fill"></i>`});
 logoutElem.addEventListener('mouseover', () => {logoutElem.innerHTML = `<i class="header-icon bi bi-door-open-fill"></i>`});
