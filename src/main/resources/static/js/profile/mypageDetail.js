c5db630a6c45be1f261b975cce2fc79350a323cc
HyeonJin-Jang <xxhyeonj1nxx@naver.com>
2021-08-16 01:48:04 +0900

    리스트 디테일 작업중

--- src/main/resources/static/js/profile/mypageDetail.js
+++ src/main/resources/static/js/profile/mypageDetail.js
@@ -1,3 +1,4 @@
+const mypageDetailModalElem = document.querySelector('#mypageDetailModal');
 const modalTitleElem = document.querySelector('#mypageDetailModal > #modalTitle');
 const modalBodyElem = document.querySelector('#mypageDetailModal > #modalList');
 const imgBtnListElem = document.querySelector('#imgBtnList');
@@ -71,13 +72,81 @@ const detailObj = {
             });
     },
     setMypageDetail : function (data) {
+        //정보 넣어놓기
+        mypageDetailModalElem.dataset.fid = `${data.feed_id}`;
+        mypageDetailModalElem.dataset.uid = `${data.users_id}`;
+        mypageDetailModalElem.dataset.regdt = `${data.feed_regdt}`;
+
         //프로필 [modalTitleElem]
-        //사진 [imgBtnListElem] , [imgListElem]
+        modalTitleElem.innerHTML = `
+            <img src="/pic/profile/${data.users_id}/${data.users_img}"
+            class="profileRadius rem_wh3"
+            onError="this.src=/img/profile/defaultProfile.png">
+            <span>${data.users_nickname}</span>
+        `;
+
+        //사진 [imgBtnListElem] , [imgListElem] , 정보 넣어놓기
+        for(let i=0; i<data.contentsList.length; i++) {
+            const item = data[i];
+            const btnId = `imgBtn${i}`;
+            const divId = `imgDiv${i}`;
+
+            //버튼
+            const imgBtn = document.createElement('button');
+            imgBtn.type = 'button';
+            imgBtn.id = btnId;
+            imgBtn.dataset.bsTarget = '#carouselExampleIndicators';
+            imgBtn.dataset.bsSlideTo = `${i}`;
+            imgBtn.setAttribute('aria-label',`Slide ${i+1}`);
+            if (i===0) {imgBtn.className = "active"; imgBtn.setAttribute('aria-current','true');}
+            imgBtnListElem.append(imgBtn);
+
+            //사진
+            const imgDiv = document.createElement('div');
+            imgDiv.className = 'carousel-item';
+            imgDiv.id = divId;
+            imgDiv.dataset.fid = `${item.feed_id}`;
+            imgDiv.dataset.cid = `${item.contents_id}`;
+            if (i===0) {imgDiv.classList.add('active')}
+            imgDiv.innerHTML = `
+                    <img src="/pic/feed/${item.feed_id}/${item.contents_img}" 
+                    class="mypageImg wh400 d-block w-100"
+                    onError="this.src=/img/feed/error.png">
+            `;
+            imgListElem.append(imgDiv);
+        }
+
         //글 [ctntElem]
+        ctntElem.innerText = `${data.feed_ctnt}`;
+
         //댓글 [icnListElem]
-        this.setTableScrollInfinity();
+        //this.setTableScrollInfinity();
         this.getCmtList(1);
+
         //아이콘 [icnListElem]
+        const favIcn = document.createElement('i');
+        favIcn.className = 'bi';
+        const cmtIcn = document.createElement('i');
+        cmtIcn.className = 'bi';
+        //const favCnt = document.createElement('span');
+        //favCnt.innerText = `${data.favCnt}`;
+        //const cmtCnt = document.createElement('span');
+        //cmtCnt.innerText = `${data.cmtCnt}`;
+
+        if (this.isFav>0) {
+            favIcn.classList.add('bi-heart-fill');
+        } else {
+            favIcn.classList.add('bi-heart');
+        }
+        if (this.isCmt>0) {
+            cmtIcn.classList.add('bi-chat-left-quote-fill');
+        } else {
+            cmtIcn.classList.add('bi-chat-left-quote');
+        }
+
+        icnListElem.innerHTML = `
+            ${favIcn}<span>${data.favCnt}</span>${cmtIcn}<span>${data.cmtCnt}</span>
+        `;
     },
     getCmtList : function(page) {
         this.showtableLoading();
